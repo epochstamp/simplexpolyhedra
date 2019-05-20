Fixed bug about sign

fqiagent.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import ExtraTreesRegressor, RandomForestRegressor, HistGra
 from simpolyhedra import SimPolyhedra
 import polyhedronutils as poly
 from copy import deepcopy
-from joblib import dump
+from joblib import dump, load
 import multiprocessing
 
 """
@@ -22,7 +22,7 @@ class FQI_Agent(object):
 
     def __init__(self, env, d_prob=2.0, feature_mode=1):
         self.env = env
-        self.RC = ExtraTreesRegressor(max_features=0.75, n_estimators=1000, n_jobs=12)
+        self.RC = ExtraTreesRegressor(n_estimators=500, n_jobs=12)
         self.LS = None
         self.d_prob = d_prob
         self.cartesian_SA = None
@@ -192,11 +192,13 @@ class FQI_Agent(object):
             self.RC.fit(inp,out)
         
 
-    def test(self):
+    def test(self, policy_tree_file=None):
         success_rate = 0
         K = 10
         N = 150
         regret_lst = []
+        if policy_tree_file is not None:
+            self.RC = load(policy_tree_file)
         print("Test on ", K, " random basis, limit of ",N," steps : ")
         for _ in range(K):
             self.env.reset()
@@ -207,7 +209,8 @@ class FQI_Agent(object):
 
             #Firstly compute the optimal path
             while not done and i < N:
-                _, _, done, _ = self.env.step(self.env.dantzigAction())
+                act = self.env.dantzigAction()
+                _, _, done, _ = self.env.step(act)
                 i += 1
             optimal_steps = i
             print("True optimal path is ",optimal_steps," steps !")
@@ -217,9 +220,16 @@ class FQI_Agent(object):
             i = 0
             while not done and i < N:
                 available_acts = env_2.getAvailableActions()
-                lst_pred = [env_2.features(a, mode=self.mode) for a in available_acts]
+                lst_pred = np.vstack([env_2.features(a, mode=self.mode) for a in available_acts])
                 values = self.RC.predict(lst_pred)
                 act = available_acts[np.argmax(values)]
+                #print (np.max(values))
+                #print(act)
+                features = env_2.features(act, mode=self.mode) 
+                #print(features[27], features[28],features[29], features[-3])
+                time.sleep(1)
+               
+                #print(self.env.features(act, mode=self.mode))
                 _, _, done, _ = env_2.step(act)
                 i += 1
             if not done:

simpolyhedra.py
@@ -63,7 +63,7 @@ class SimPolyhedra():
     """
     Discount factor
     """
-    def gamma(self): return 0.95
+    def gamma(self): return 0.995
 
     def initFeatures(self,tol = 1e-8):
         self.staticFeatures = np.zeros([27,self.n])
@@ -283,9 +283,9 @@ class SimPolyhedra():
         cA = abs(self.state[0,1+act])/self.state[mask,1+act]
         min_cA = np.min(cA)
         max_cA = np.max(cA)
-        signmin_cA = np.sign(min_cA)
+        signmin_cA = sign(min_cA)
         absmin_cA = np.abs(min_cA)
-        signmax_cA = np.sign(max_cA)
+        signmax_cA = sign(max_cA)
         absmax_cA = np.abs(max_cA)
         poscond = self.state[0,1+act] >= 0
         negcond = self.state[0,1+act] < 0
@@ -594,10 +594,14 @@ class SimPolyhedra():
             self.basis[self.rowVar[e]] = False
             self.basis[act] = True
             self.rowVar[e] = act
+
+            
             
             if trueStep:
                 self.entered[act] += 1
                 self.steps += 1
+
+            reward = 0#0.01 * -self.entered[act]/self.steps - 0.01 * sign(self.state[0,1+act])
             
             # Termination if optimal
             if self.isOptimal():