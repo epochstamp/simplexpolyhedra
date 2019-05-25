d93fe15f7330ec5424111a6ad41b3f91ed35f201
SamAit <samait@localhost.localdomain>
2019-05-25 16:35:02 +0200

    Final fixes for slurm launching in supercomputers

--- patternized_slurm.sh
+++ patternized_slurm.sh
@@ -6,7 +6,6 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=6144 # 6GB
-#SBATCH --partition=defq
 
 
 srun python fqiagent.py --n-episodes %nepisodes --horizon-time %horizontime --max-njobs 12 --estimator %estimator --overwrite-mode a --geotype unitcube --vertices 50 --feature-mode %featuremode --bias-exploration-coeff %biasexplorationcoeff --seed %seed --envs-tests unitcube.cfg --n-episodes-test 40 
