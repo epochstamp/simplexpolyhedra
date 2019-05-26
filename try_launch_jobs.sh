2b288b623dae4b80be2736e02a9533626615773e
SamAit <samait@localhost.localdomain>
2019-05-26 18:59:53 +0200

    launch job trial with job inexec detection

--- try_launch_jobs.sh
+++ try_launch_jobs.sh
@@ -3,5 +3,13 @@
 for dir in slurm_jobs/*; do
   f=`basename $dir`
   fname="${f%.*}"
-  sbatch $dir
+  jobname=`cat "$dir" | sed -n 's/.*#SBATCH --job-name=//p'`
+  grep_result=`squeue -u samait 2> /dev/null | grep jobname`
+  job_not_launched=0
+  if [ -z "$grep_result" ]; then
+    echo "Job not launched yet"
+  else
+    echo "Job launched"
+  fi
+  #sbatch $dir
 done
