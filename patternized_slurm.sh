2925066433721f6e611bfa2f82dba33cdc2d4a4e
Samy Aittahar <saittahar@uliege.be>
2019-05-27 13:04:26 +0200

    increased needed memory per cpu

--- patternized_slurm.sh
+++ patternized_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=12
 #
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=6144 # 6GB
+#SBATCH --mem-per-cpu=8192 # 8GB
 
 
 srun python fqiagent.py --n-episodes %nepisodes --horizon-time %horizontime --max-njobs 12 --estimator %estimator --overwrite-mode a --geotype unitcube --vertices 50 --feature-mode %featuremode --bias-exploration-coeff %biasexplorationcoeff --seed %seed --envs-tests unitcube.cfg --n-episodes-test 40 --maximum-time-exec-hours 6 --q-iterations 100
